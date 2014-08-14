58c9d838577969e9d2ae9ed8c905ccfb65434f3f
Tomas Celizna <tomas.celizna@gmail.com>
2014-08-14 18:15:02 +0200

    dummy controller

--- test/mongoid_traffic/controller_additions_test.rb
+++ test/mongoid_traffic/controller_additions_test.rb
@@ -8,18 +8,19 @@ module MongoidTraffic
     before(:each) do
       @controller_class = Class.new
       @controller = @controller_class.new
+      def @controller_class.helper_method(name); end
       @controller_class.send(:include, MongoidTraffic::ControllerAdditions)
     end
 
     describe '.log_traffic' do
-      # it 'logs with :user_agent'
-      # it 'logs with :referer'
-      # it 'logs with :ip_address'
-      # it 'logs with :unique_id'
+      it 'logs with :user_agent'
+      it 'logs with :referer'
+      it 'logs with :ip_address'
+      it 'logs with :unique_id'
     end
 
     describe '.log_scoped_traffic' do
-      # it 'infers scope from request path'
+      it 'infers scope from request path'
     end
 
   end
